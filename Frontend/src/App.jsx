cd46ec4f1ee3ec4ce5e41d8986c32a408123a47e
raviswamiii <raviswamisamsung@gmail.com>
2025-09-06 11:19:45 +0530

    settingUpProject

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { Card } from './components/Card'
+import { Route, Routes } from 'react-router-dom'
+import { SignIn } from './components/SignIn'
+import { CreateCard } from './components/CreateCard'
+import { SignUp } from './components/SignUp'
 
 export const App = () => {
   return (
-    <div className='bg-amber-500'>App</div>
+    <div className='h-screen flex justify-center items-center bg-black p-4'>
+      <Routes>
+        <Route path='/card' element={<Card/>} />
+        <Route path='/' element={<SignIn/>} />
+        <Route path='/signUp' element={<SignUp/>} />
+        <Route path='/createCard' element={<CreateCard/>} />
+      </Routes>
+    </div>
   )
 }
---
+++ Frontend/src/components/Card.jsx
@@ -0,0 +1,7 @@
+import React from 'react'
+
+export const Card = () => {
+  return (
+    <div className='h-110 w-75 border-1 bg-black text-white rounded-xl'>Card</div>
+  )
+}
