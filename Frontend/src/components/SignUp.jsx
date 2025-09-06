3a56d7d719420d584a7f65cc4a4ce25125af3efd
raviswamiii <raviswamisamsung@gmail.com>
2025-09-06 15:46:26 +0530

    frontendUserAuth

--- Frontend/src/components/SignUp.jsx
+++ Frontend/src/components/SignUp.jsx
@@ -3,8 +3,8 @@ import { NavLink } from "react-router-dom";
 
 export const SignUp = () => {
   return (
-    <div className="flex flex-col justify-center items-center gap-3">
-      <h1 className="bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-800 bg-clip-text text-transparent text-2xl font-bold">
+    <div className="flex flex-col justify-center items-center">
+      <h1 className="mb-2 bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-800 bg-clip-text text-transparent text-2xl font-bold">
         Create your account
       </h1>
       <div className="bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-800 p-[2px] rounded-xl">
@@ -13,19 +13,19 @@ export const SignUp = () => {
           className="bg-black rounded-xl p-4 text-white text-lg flex flex-col gap-3"
         >
           <input
-            className="border px-4 py-2 rounded-lg"
-            type="text"
+            className="border px-4 py-2 rounded-lg outline-none"
+            type="name"
             placeholder="Enter your name..."
           />
           <input
-            className="border px-4 py-2 rounded-lg"
-            type="text"
-            placeholder="Enter your name..."
+            className="border px-4 py-2 rounded-lg outline-none"
+            type="email"
+            placeholder="Enter your email..."
           />
           <input
-            className="border px-4 py-2 rounded-lg"
-            type="text"
-            placeholder="Enter your name..."
+            className="border px-4 py-2 rounded-lg outline-none"
+            type="password"
+            placeholder="Enter your password..."
           />
           <button
             type="submit"
@@ -35,7 +35,7 @@ export const SignUp = () => {
           </button>
         </form>
       </div>
-      <NavLink to={'/signIn'} className="bg-gradient-to-r from-yellow-200 via-yellow-500 to-yellow-800 bg-clip-text text-transparent text-sm">Sign In if you already have an account</NavLink>
+      <NavLink to={'/signIn'} className="bg-gradient-to-r from-yellow-200 via-yellow-500 to-yellow-800 bg-clip-text text-transparent text-sm mt-1">Sign In if you already have an account</NavLink>
     </div>
   );
 };
