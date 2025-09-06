9c9169cb7ac3383b39faa7ccd84288c81f2bd176
raviswamiii <raviswamisamsung@gmail.com>
2025-09-06 21:25:49 +0530

    signUp

--- Frontend/src/components/SignUp.jsx
+++ Frontend/src/components/SignUp.jsx
@@ -1,7 +1,16 @@
-import React from "react";
+import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 
 export const SignUp = () => {
+  const [name, setName] = useState();
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+
+  const onSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log("Form submitted.");
+  };
+
   return (
     <div className="flex flex-col justify-center items-center">
       <h1 className="mb-2 bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-800 bg-clip-text text-transparent text-2xl font-bold">
@@ -10,11 +19,12 @@ export const SignUp = () => {
       <div className="bg-gradient-to-r from-yellow-300 via-yellow-500 to-yellow-800 p-[2px] rounded-xl">
         <form
           action=""
+          onSubmit={onSubmitHandler}
           className="bg-black rounded-xl p-4 text-white text-lg flex flex-col gap-3"
         >
           <input
             className="border px-4 py-2 rounded-lg outline-none"
-            type="name"
+            type="text"
             placeholder="Enter your name..."
           />
           <input
@@ -35,7 +45,12 @@ export const SignUp = () => {
           </button>
         </form>
       </div>
-      <NavLink to={'/signIn'} className="bg-gradient-to-r from-yellow-200 via-yellow-500 to-yellow-800 bg-clip-text text-transparent text-sm mt-1">Sign In if you already have an account</NavLink>
+      <NavLink
+        to={"/signIn"}
+        className="bg-gradient-to-r from-yellow-200 via-yellow-500 to-yellow-800 bg-clip-text text-transparent text-sm mt-1"
+      >
+        Sign In if you already have an account
+      </NavLink>
     </div>
   );
 };
