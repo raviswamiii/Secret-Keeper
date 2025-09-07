edef41d7c89306592ab3f9986a57d97f3f043f3f
raviswamiii <raviswamisamsung@gmail.com>
2025-09-07 11:55:47 +0530

    userModel

--- Backend/database/mongoDB.js
+++ Backend/database/mongoDB.js
@@ -1,11 +1,11 @@
 import mongoose from "mongoose";
 
 const databaseConnection = () => {
-    mongoose.connection.on("connected", () => {
-        console.log("Connected to database.")
-    })
+  mongoose.connection.on("connected", () => {
+    console.log("Connected to database.");
+  });
 
-    mongoose.connect(process.env.MONGODB_URI);
-}
+  mongoose.connect(process.env.MONGODB_URI);
+};
 
-export default databaseConnection
+export default databaseConnection;
--- Backend/server.js
+++ Backend/server.js
@@ -7,5 +7,5 @@ dotenv.config();
 databaseConnection();
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000")
-})
+  console.log("Server is running on port 3000");
+});
