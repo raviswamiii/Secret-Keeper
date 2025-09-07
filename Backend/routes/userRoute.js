157fde7623b53608e2a4c2f2e82b4aa9087841bc
raviswamiii <raviswamisamsung@gmail.com>
2025-09-07 15:12:11 +0530

    userRoute

---
+++ Backend/routes/userRoute.js
@@ -0,0 +1,8 @@
+import express from "express";
+import userRegister from "../controllers/userController";
+
+const userRouter = express.Router();
+
+userRouter.post("/register", userRegister);
+
+export default userRouter;
