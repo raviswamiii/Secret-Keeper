f3fa5419207f20676e22045a3d4e5c0b665a4279
raviswamiii <raviswamisamsung@gmail.com>
2025-09-10 15:28:08 +0530

    frontendSetUp

--- Backend/routes/userRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import userRegister from "../controllers/userController.js";
-
-const userRouter = express.Router();
-
-userRouter.post("/register", userRegister);
-
-export default userRouter;
--- Frontend/src/components/CreateCard.jsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const CreateCard = () => {
-  return (
-    <div>CreateCard</div>
-  )
-}
