2f4d4a550191b18c8db93fa542c398aeca889525
raviswamiii <raviswamisamsung@gmail.com>
2025-09-10 18:04:57 +0530

    databaseSetUp

--- Backend/server.js
+++ Backend/server.js
@@ -1,6 +1,11 @@
 import express from "express";
-const app =  express();
+const app = express();
+import dotenv from "dotenv";
+import databaseConnection from "./database/MongoDB.js";
+
+dotenv.config();
+databaseConnection();
 
 app.listen(3000, () => {
-    console.log("Server is running on port 3000.")
-})
+  console.log("Server is running on port 3000.");
+});
