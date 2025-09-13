8309f78d2a7b74515a20ae13deff55842beef87f
raviswamiii <raviswamisamsung@gmail.com>
2025-09-13 12:58:21 +0530

    userReginstrationUI

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,17 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import { Home } from "./pages/Home";
+import { UserRegister } from "./pages/UserRegister";
+import { UserLogin } from "./pages/UserLogin";
 
 export const App = () => {
   return (
-    <div className='bg-green-400'>App</div>
-  )
-}
+    <div>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/userRegister" element={<UserRegister />} />
+        <Route path="/userLogin" element={<UserLogin />} />
+      </Routes>
+    </div>
+  );
+};
---
+++ Frontend/src/pages/UserRegister.jsx
@@ -0,0 +1,83 @@
+import axios from "axios";
+import React, { useState } from "react";
+import { FiLogIn } from "react-icons/fi";
+import { Link, useNavigate } from "react-router-dom";
+
+export const UserRegister = () => {
+  const [name, setName] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState("");
+  const backendURL = import.meta.env.VITE_BACKEND_URL;
+  const navigate = useNavigate();
+
+  const onSubmitHandler = async (event) => {
+    event.preventDefault();
+
+    try {
+      const userData = { name, email, password };
+
+      const response = await axios.post(
+        backendURL + "/user/register",
+        userData
+      );
+
+      if (response.data.success) {
+        localStorage.getItem("accessToken");
+        navigate("/");
+      }
+    } catch (error) {
+      setError(error.response?.data?.message || "Something went wrong.");
+    }
+  };
+  return (
+    <div className="bg-black h-screen flex flex-col justify-center items-center">
+      <Link
+        to={"/userLogin"}
+        className="absolute top-3 right-3 bg-gradient-to-r from-yellow-200 via-yellow-500 to-yellow-800 flex items-center gap-1 px-2 py-1 rounded-md"
+      >
+        <p className="text-lg font-bold">Log In</p>
+        <FiLogIn className="text-2xl" />
+      </Link>
+      <h1 className="bg-gradient-to-r from-yellow-200 via-yellow-500 to-yellow-800 bg-clip-text text-transparent font-bold text-2xl mb-2">
+        Create your account
+      </h1>
+      <div className="bg-gradient-to-r from-yellow-200 via-yellow-500 to-yellow-800 rounded-lg p-[1px] w-75">
+        <form
+          action=""
+          onSubmit={onSubmitHandler}
+          className="bg-black rounded-lg p-3 text-white flex flex-col gap-4"
+        >
+          <input
+            className="outline-none border border-white p-3 rounded-lg"
+            type="text"
+            placeholder="Enter your name..."
+            value={name}
+            onChange={(event) => setName(event.target.value)}
+          />
+          <input
+            className="outline-none border border-white p-3 rounded-lg"
+            type="email"
+            placeholder="Enter your name..."
+            value={email}
+            onChange={(event) => setEmail(event.target.value)}
+          />
+          <input
+            className="outline-none border border-white p-3 rounded-lg"
+            type="password"
+            placeholder="Enter your name..."
+            value={password}
+            onChange={(event) => setPassword(event.target.value)}
+          />
+          <button
+            type="submit"
+            className="bg-gradient-to-r from-yellow-200 via-yellow-500 to-yellow-800 p-3 rounded-lg font-bold text-xl"
+          >
+            Sign Up
+          </button>
+        </form>
+      </div>
+      <p className="text-red-600 text-sm mt-1">{error}</p>
+    </div>
+  );
+};
