544cba1ced5df517acf4978373a757b7eb2deed8
raviswamiii <raviswamisamsung@gmail.com>
2025-09-13 13:46:51 +0530

    databaseConnected

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,10 @@
 import express from "express";
 const app = express();
+import dotenv from "dotenv";
+import databaseConnection from "./database/MongoDB.js";
+
+dotenv.config();
+databaseConnection();
 
 app.listen(3000, () => {
     console.log("Server is running on port 3000.")
