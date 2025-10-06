ec41a3824694637ea755c42f76430d55fc4e40f7
raviswamiii <raviswamisamsung@gmail.com>
2025-10-06 16:52:56 +0530

    userController

---
+++ Backend/controllers/userController.js
@@ -0,0 +1,93 @@
+import jwt from "jsonwebtoken";
+import bcrypt from "bcrypt";
+import validator from "validator";
+import userModel from "../models/userModel";
+
+const createToken = (id) => {
+  return jwt.sign({ id }, process.env.JWT_SECRET, { expiresIn: "1d" });
+};
+
+const signUp = async (req, res) => {
+  try {
+    const { name, email, password } = req.body;
+
+    if (!name || !email || !password)
+      return res
+        .status(400)
+        .send({ success: false, message: "All fields are required." });
+
+    const exists = await userModel.findOne({ email });
+
+    if (exists)
+      return res
+        .status(400)
+        .send({ success: false, message: "User already exists." });
+
+    if (!validator.isEmail(email))
+      return res
+        .status(400)
+        .send({ success: false, message: "Please enter valid email." });
+
+    if (!validator.isStrongPassword(password))
+      return res.status(400).send({
+        success: false,
+        message:
+          "Password must be 8 character's long including uppercase, lowercase, number and symbol.",
+      });
+
+    const salt = bcrypt.genSalt(10);
+    const hashedPassword = bcrypt.hash(password, salt);
+
+    const newUser = await userModel({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    const user = newUser.save();
+    const token = createToken(user._id);
+
+    return res
+      .status(200)
+      .send({ success: true, message: "User registered successfully.", token });
+  } catch (error) {
+    console.error("SignUp error:", error.message);
+    return res.status(500).send({ success: false, message: "Server error." });
+  }
+};
+
+const signIn = async (req, res) => {
+  try {
+    const { email, password } = req.body;
+
+    if ((!email, !password))
+      return res
+        .status(400)
+        .send({ success: false, message: "All fields are required." });
+
+    const user = await userModel.findOne({ email });
+
+    if (!user)
+      return res
+        .status(400)
+        .send({ success: false, message: "Invalid credentials." });
+
+    const isMatch = bcrypt.compare(password, user.password);
+
+    if (!isMatch)
+      return res
+        .status(400)
+        .send({ success: false, message: "Invalid credentials." });
+
+    const token = createToken(user._id);
+
+    return res
+      .status(200)
+      .send({ success: true, message: "User logged in successfully." });
+  } catch (error) {
+    console.error("SignIn error:", error.message);
+    return res.status(500).send({ success: false, message: "Server error." });
+  }
+};
+
+export default { signUp, signIn };
