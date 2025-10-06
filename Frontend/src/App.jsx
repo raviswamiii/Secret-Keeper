1d6885b8987f62facef02dd426e0d818bb8db8c9
raviswamiii <raviswamisamsung@gmail.com>
2025-10-06 15:18:33 +0530

    frontendRouting

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,7 +1,17 @@
 import React from 'react'
+import { Route, Routes } from 'react-router-dom'
+import { SignIn } from './pages/SignIn'
+import { Home } from './pages/Home'
+import { SignUp } from './pages/SignUp'
 
 export const App = () => {
   return (
-    <div className='bg-green-600'>App</div>
+    <div>
+      <Routes>
+        <Route path='/' element={<SignIn/>}/>
+        <Route path='/home' element={<Home/>}/>
+        <Route path='/signUp' element={<SignUp/>}/>
+      </Routes>
+    </div>
   )
 }
