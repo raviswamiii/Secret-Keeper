42088ed5e08a20740f13048a62ee40a6e0cb6f4a
raviswamiii <raviswamisamsung@gmail.com>
2025-10-06 16:06:09 +0530

    mongodbSetUp

--- Backend/server.js
+++ Backend/server.js
@@ -1,5 +1,10 @@
 import express from "express";
 const app = express();
+import databaseConnection from "./config/mongodb.js";
+import dotenv from "dotenv";
+
+dotenv.config();
+databaseConnection();
 
 app.listen(3000, ()=>{
     console.log("Server is running on port 3000.")
